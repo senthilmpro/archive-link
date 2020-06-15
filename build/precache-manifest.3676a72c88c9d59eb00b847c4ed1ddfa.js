1726bbd6b160043bd397ee800014c4668fa4dc06
skmgit <skm.symc@gmail.com>
2020-06-14 17:29:19 -0700

    init changes

---
+++ build/precache-manifest.3676a72c88c9d59eb00b847c4ed1ddfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9714e574946e68bb1f6cf50532dc9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ee8a67d0a9865ede17",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e772b41fc021d4768e4",
+    "url": "/static/js/2.a6b45129.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6b45129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ee8a67d0a9865ede17",
+    "url": "/static/js/main.48febd53.chunk.js"
+  },
+  {
+    "revision": "444b6a81fdecd16dab05",
+    "url": "/static/js/runtime-main.021e0fa7.js"
+  }
+]);
